3723b5e63fe754032b05cd892299465607dd4cb6
lheston <loomisheston@hotmail.com>
2016-08-03 06:14:04 +0000

    working!

--- firstApi/settings.py
+++ firstApi/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework', # enable REST framework
     'firstApi',
+    'apitest',
 ]
 
 MIDDLEWARE = [
@@ -123,6 +124,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/firstApi/'
+STATIC_URL = '/apitest/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
